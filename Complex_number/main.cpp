f1ff49bc03679c22a29449cdc955e346a75c4373
Mike <mihailqqqq@gmail.com>
2016-09-15 17:46:22 +0300

    small update

--- Complex_number/main.cpp
+++ Complex_number/main.cpp
@@ -6,12 +6,6 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    cout << "Введите";
-    ComplexNum a = ComplexNum(-1, 2);
-    ComplexNum b = ComplexNum(7, -3);
-
-    a = a / b;
-    a.print();
 
     return 0;
 }
